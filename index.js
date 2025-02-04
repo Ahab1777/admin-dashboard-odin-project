811e13bd3ac9962efc0d9a59bfa64070d79a9dd7
Leonardo de Pinho <pinholeonardo1777@gmail.com>
2025-02-04 11:24:01 -0300

    exercise done - needs improvements

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+
+document.querySelectorAll('.project-content').forEach(project => {
+    project.innerText += 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel deleniti vero quod accusantium nisi. '
+})
+
+document.querySelectorAll('.project-title').forEach(project => {
+    project.innerText += 'Project Title'
+})
+
